ad655ff7946faaf12ddeb6325cac5c57a1c4b05f
Pranjal Rai <pranjalbhs@gmail.com>
2021-09-30 21:11:36 +0530

    started homepage

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -3,11 +3,22 @@ import Navbar from "./Navbar"
 
 function Home(){
     return (
-      <div>
+      <div className="container-fluid">
+        <div className="mt-6 header h-668px" style={{backgroundImage: "url(/homebg.jpg)", backgroundSize: "cover"}}>
         <Navbar />
-        <div className="text-center mt-6">This is the Home Page</div>
+        <div className="grid justify-items-center">
+          {/* <div></div> */}
+          <div className="h-80 opacity-80 rounded-md w-568px bg-black mt-24 z-0">
+            <div className="text-white opacity-100 z-10 text-2xl px-20 font-heading mt-8">Need a vacation? Don't worry we got you!</div>
+            <div className="w-96 h-36 bg-lightgreen ml-20 mt-4 rounded-md z-20 opacity-100">
+            <input type="text" className="z-30 w-72 ml-4 mt-4 bg-white h-12 z-20 rounded-md opacity-100" />
+            </div>
+          </div>
+          {/* <div></div> */}
+          </div>
+          </div>
+        
       </div>
-  
     );
 }
 
