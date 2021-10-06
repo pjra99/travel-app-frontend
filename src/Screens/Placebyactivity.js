86d93000e879c503cd97dbeec63ab71e25bbe0bc
Pranjal Rai <pranjalbhs@gmail.com>
2021-10-06 17:06:58 +0530

    added 'what's your mood setion'

--- src/Screens/Placebyactivity.js
+++ src/Screens/Placebyactivity.js
@@ -1,8 +1,10 @@
 import react from "react";
 
-function Placebyactivity(){
+function Placebyactivity(props){
     return(
-        <div className="h-52 w-48 bg-lightgreen ml-28 rounded-md p-2" style={{backgroundImage: "url(/camping.jpg)"}}>
+        <div className="h-72 w-56 bg-lightgreen rounded-md p-2 mx-6">
+            <img className="h-60 w-full rounded-md" src={props.img} />
+            <div className="text-center mt-2 text-green font-medium">{props.text}</div>
         </div>
     );
 }
