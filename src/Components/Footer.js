ef2fb5a950e3ff058ee51979cd7331ad1ffeef3c
Pranjal Rai <pranjalbhs@gmail.com>
2021-10-08 17:54:44 +0530

    completed the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,6 @@
 function Footer(){
     return (
-        <div className="flex flex-wrap mt-16 bg-lightgreen text-green py-6">
+        <div className="flex flex-wrap mt-16 bg-lightgreen text-green py-6 w-screen">
             <div className="w-68 ml-32">
                 <h2 className="text-xl font-heading mb-4">TravelEzy</h2>
                 <p className="font-semibold">We created a seamless experience for you 
@@ -23,6 +23,12 @@ to help you discover and explore this world better.</p>
             <div className="ml-28">
                <div className="flex"> <div>Subscribe to our newsletter</div> <button className="text-white bg-green ml-3 p-2 rounded-md"><img src="next.svg" /></button></div>
                 <input className="py-1 mt-2 rounded-sm w-60" type="text" placeholder="  yourmail@xyz.com"/>
+                <div className="flex justify-center mt-2">
+                    <img src="twitter.svg" className="w-9 mt-2" />
+                    <img src="facebook icon.svg" className="w-9 mt-2 ml-8" />
+                    <img src="googleplus.svg" className="w-9 mt-2 ml-8" />
+                    <img src="youtube.svg" className="w-9 mt-2 ml-8" />
+                </div>
             </div>
         </div>
     );
