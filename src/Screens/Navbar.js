1d92ebc2e8f718b6f9420e9fb832ebbdfe0a610c
Pranjal Rai <pranjalbhs@gmail.com>
2021-10-11 20:11:44 +0530

    just some uneccesary code removal

--- src/Screens/Navbar.js
+++ src/Screens/Navbar.js
@@ -1,36 +1,6 @@
 import React from "react"
 import {Link} from 'react-router-dom'
 function Navbar(){
-
-    function changeBackground(num){
-        console.log("Clicked")
-        document.getElementById("home").style.background = "white"
-       document.getElementById("home").style.color = 'black'
-       document.getElementById("blogs").style.background = "white"
-       document.getElementById("blogs").style.color = 'black'
-       document.getElementById("upload").style.background = "white"
-        document.getElementById("upload").style.color = 'black'
-        document.getElementById("login").style.background = "white"
-        document.getElementById("login").style.color = 'black'
-
-     if(num==1){
-       document.getElementById("home").style.background = "black"
-       document.getElementById("home").style.color = 'white'
-     }
-     if(num==2){
-        document.getElementById("blogs").style.background = "black"
-        document.getElementById("blogs").style.color = 'white'
-     }
-     if(num==3){
-        document.getElementById("upload").style.background = "black"
-        document.getElementById("upload").style.color = 'white'
-     }
-     if(num==4){
-        document.getElementById("login").style.background = "black"
-        document.getElementById("login").style.color = 'white'
-     }
-    // alert("hi")
-    }
     return (
 <div className="grid grid-cols-10 absolute mt-10 w-screen font-body bg-white">
     <div></div>
@@ -38,10 +8,10 @@ function Navbar(){
     <div></div>
     <div></div>
     <div></div>
-    <Link to="/home" onClick={()=>changeBackground(1)}><div id="home"  >Home</div></Link>
-    <Link to="/blogs" onClick={()=>changeBackground(2)}><div id="blogs"   >Blogs</div></Link>
-   <Link to="/upload" onClick={()=>changeBackground(3)}><div id="upload"  >Upload </div></Link> 
-   <Link to="/signin" onClick={()=>changeBackground(4)}> <div id="login"  >Log In</div></Link>
+    <Link to="/home"><div id="home"  >Home</div></Link>
+    <Link to="/blogs"><div id="blogs"   >Blogs</div></Link>
+   <Link to="/upload"><div id="upload"  >Upload </div></Link> 
+   <Link to="/signin"> <div id="login"  >Log In</div></Link>
     <div></div>
 </div>
     );
