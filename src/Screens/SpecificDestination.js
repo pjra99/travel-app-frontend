7887982d8bf8b68cabcd12edfb1ba1d7d16c6476
Pranjal Rai <pranjalbhs@gmail.com>
2021-10-28 18:48:43 +0530

    improved css

--- src/Screens/SpecificDestination.js
+++ src/Screens/SpecificDestination.js
@@ -9,21 +9,24 @@ return(
      <SpecificPlaceHeader />
      <div className="font-heading text-3xl text-center mt-16"><span className="text-green">H</span>otels</div>
      <div className="flex mt-10">
-       <div className="flex-1 ml-48">
+       <div className="flex-1 ml-36">
          <div className=""><HotelCard /></div>
          <div className="mt-10"><HotelCard /></div>
        </div>
-       <div className="flex-1 -ml-32">
+       <div className="flex-1 -ml-24">
        <div className="mt-10"><HotelCard /></div>
        <div className="mt-10"><HotelCard /></div>
        </div>
-       <div className="flex-1 -ml-32 pr-16 ">
+       <div className="flex-1">
 <div className="text-3xl font-heading">      <span className="text-green">D</span>iscover the Best Hotel 
 Deals in Nanitaal</div>
 <div className="mt-16">Get instant confirmation, last minute hotel deals. Find Hotels For Tonight Near to You. No cancellation fees. Exclusive deals. From Fasntastic Hotels To Modern Apartments. Easy and Secure payment. Best Price Guarantee. Free cancellation. Easy, Fast and Secure.</div>
+<button className="bg-green text-white text-xl pt-2 font-heading text-center py-2 px-8 rounded-md mt-16">Explore more</button>
        </div>
-
+       <div className="w-32"></div>
+       
      </div>
+     
    </div>
 )
 }
