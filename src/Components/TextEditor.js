73e1baa4de84fe8d59e8571e375190dc705f1382
pjra99 <pranjalbhs@gmail.com>
2021-11-15 19:25:01 +0530

    made the text editor for blogs

---
+++ src/Components/TextEditor.js
@@ -0,0 +1,62 @@
+function TextEditor() {
+  return (
+    <div className="flex-col justify-between mt-16 rounded-md border-2 border-lightgrey mx-36">
+      <div className="flex justify-center h-12 pt-3 mb-2 bg-black text-white rounded-t-md font-heading text-xl">
+        Been to this place? Write a Blog!
+      </div>
+      <div className="px-2">
+        <textarea
+          className="w-full h-10 border-2 border-lightgrey "
+          placeholder="   Title"
+        />
+        <textarea
+          className="w-full h-68 border-2 border-lightgrey "
+          placeholder="  (Body)"
+        />
+      </div>
+      <div className="h-12  bg-lightgreen flex flex-wrap  justify-between pl-4 rounded-b-md">
+        <div className="pt-3 ">
+          Font Size
+          <select name="font-size" id="font-size" className="ml-2">
+            <option value="16">16</option>
+            <option value="18">18</option>
+            <option value="20">20</option>
+            <option value="22">22</option>
+          </select>
+        </div>
+        <div className="pt-3">
+          Font Family{" "}
+          <select name="font-style" id="font-style" className="ml-1">
+            <option value="IBM Plex Sans">IBM Plex Sans</option>
+            <option value="Mono">Mono</option>
+            <option value="Times New Roman">Times New Roman</option>
+            <option value="Comfortaa">Comfortaa</option>
+          </select>
+        </div>
+        <div className="flex w-36 ml-10 flex-wrap justify-between">
+          <button>
+            <img src="leftalign.svg" className="h-6" />
+          </button>
+          <button>
+            <img src="centeralign.svg" className="h-6" />
+          </button>
+          <button>
+            <img src="rightalign.svg" className="h-6" />
+          </button>
+        </div>
+        <div className="flex w-36 ml-10 flex-wrap justify-between mr-10">
+          <button>
+            <img src="underlineicon.svg" className="h-6" />
+          </button>
+          <button>
+            <img src="italicsicon.svg" className="h-5" />
+          </button>
+          <button>
+            <img src="boldicon.svg" className="h-6" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default TextEditor;
--- src/Screens/Blogs.js
+++ src/Screens/Blogs.js
@@ -2,36 +2,31 @@ import SpecificPlaceHeader from "../Components/SpecificPlaceHeader";
 import PlaceCard from "../Components/Blogcard";
 import Footer from "../Components/Footer";
 import Blogcard from "../Components/Blogcard";
-
+import TextEditor from "../Components/TextEditor";
 
 function Blogs() {
   return (
     <div>
       <SpecificPlaceHeader />
-
-      <h1 class="text-center">text editior</h1>
+      <TextEditor />
       <div className="text-center font-heading text-3xl mt-16">
         <span className="text-green">B</span>logs
       </div>
-      <div className="flex flex-row justify-center flex-wrap">
+      <div className="flex flex-wrap justify-between md:px-28">
+        <Blogcard />
+        <Blogcard />
+        <Blogcard />
+      </div>
+      <div className="flex flex-wrap justify-between md:px-28">
         <Blogcard />
         <Blogcard />
         <Blogcard />
       </div>
 
-      <div className="flex flex-row justify-center flex-wrap">
+      <div className="flex flex-wrap justify-between md:px-28">
         <Blogcard />
         <Blogcard />
         <Blogcard />
-      </div>
-
-      <div className="flex flex-row justify-center flex-wrap">
-        <Blogcard />
-        <Blogcard />
-        <Blogcard />
-
-
-
       </div>
 
       <Footer />
